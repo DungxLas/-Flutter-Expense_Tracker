49fbae9257ca5fd22db290bc0f7b09b9ee703fb8
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-08-31 11:24:15 +0700

    31/08/2023

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,1 +1,16 @@
-class Expense {}
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+  }) : id = uuid.v4();
+}
